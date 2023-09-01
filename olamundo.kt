e1bf97d05432af08eb5de31fec1ce8a44c4d32f2
maninhomarcos <111518436+maninhomarcos@users.noreply.github.com>
2023-09-01 11:30:33 -0300

    Create olamundo.kt

---
+++ olamundo.kt
@@ -0,0 +1,25 @@
+//abrir tg fum main
+fun main() {
+  //pode usar a função print ou println(mover texto para baixo) para digitar na linha de baixo
+  println("AIN delicia");
+  println("Maravilha da roça");
+  println("16? N parece.");
+  //printar numeros sem aspas faz a soma deles
+  println(5+4);
+  //com aspas escreve eles
+  println("5+2");
+  
+  print("Hello World!");
+  print("I am learning Kotlin.");
+  println("It is awesome!");
+  //variaveis em kotlin (primeiro nome e sobrenome)
+  // criando variavel nome
+  var firstName = "Marcos";
+  //kotlin irá usar o nome colocado entre aspas 
+  println(firstName);
+  var surName = "André";
+  println(surName);
+  // var n1 irá usar o primeiro numero digitado (os numeros com aspas)e não os somados sem aspas
+  var n1=5;
+  println(n1);
+}
