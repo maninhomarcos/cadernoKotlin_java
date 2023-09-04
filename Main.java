558b0457a4d17f071d029307d8f68396cefc3ef5
maninhomarcos <111518436+maninhomarcos@users.noreply.github.com>
2023-09-04 11:37:20 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String[] args) {
+    String ola = "Ola mundão";// criar uma var de texto
+    System.out.println(ola);//imprimir a variavel na tela
+  }
+}
